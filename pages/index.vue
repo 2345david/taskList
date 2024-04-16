72a6182bb6b4feddbfbb355f5cff8a8a066c6b97
Daniel <danielvazquez245.dv@gmail.com>
2024-04-16 17:58:28 +0200

    started working

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-teal-950 flex h-screen w-screen items-center justify-center  ">
+    <div class=" max-w-[800px] w-full shadow-lg flex flex-col   ">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "index"
+})
+</script>
