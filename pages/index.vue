05da2c7184fa3619ca16bfa84210e5a601de588c
Daniel <danielvazquez245.dv@gmail.com>
2024-04-17 10:16:07 +0200

    added scroll in main container and installed sass

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="bg-teal-950 flex h-screen w-screen items-center justify-center  ">
+  <div class="bg-teal-950 flex h-screen w-screen items-center justify-center overflow-hidden ">
     <div class=" max-w-[800px] w-full shadow-lg flex flex-col   ">
-      <div class="flex flex-col p-2 gap-2 bg-emerald-200 border-4 border-white rounded "> <!-- Contenedor tareas-->
+      <div
+          class="flex flex-col p-2 gap-2 bg-emerald-200 border-4 border-white rounded overflow-y-scroll scrollbar max-h-[500px] min-h-[500px]">
+        <!-- Contenedor tareas-->
+        <task/>
+        <task/>
         <task/>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -15,3 +20,21 @@ export default defineComponent({
   name: "index"
 })
 </script>
+<style scoped lang="scss">
+.scrollbar {
+  &::-webkit-scrollbar {
+    width: 0;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #c7cbce;
+    border-radius: 10px;
+    border: 0 solid transparent;
+    cursor: pointer;
+  }
+}
+</style>
