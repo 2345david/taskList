d23956ea642b5650e00f4aa9d1ec1c643b041f3f
david <ramosdavu2006@gmail.com>
2024-04-17 10:05:14 +0200

    create a buttons delete,add and edit, create inputs and create icon, add photos fot buttons

---
+++ components/inputs.vue
@@ -0,0 +1,4 @@
+<template>
+  <input type="text" class="border border-2 border-emerald-200 bg-emerald-200 outline-none focus-within:border-lime-600 rounded-lg">
+
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div class="bg-teal-950 flex flex-col justify-center h-30 p-2">
+    <div class="w-20">
+      <NuxtImg src="task.svg"/>
+    </div>
+    <div class="flex gap-6 justify-center">
+      <inputs/>
+    </div>
+    <div class="flex gap-6">
+      <buttons
+        button-img="delete.svg"
+      />
+      <buttons
+          button-img="edit.svg"
+      />
+      <buttons
+          button-img="add.svg"
+      />
+    </div>
+  </div>
+</template>
+
+
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "index"
+})
+</script>
