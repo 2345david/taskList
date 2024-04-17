f735a1bfaeac5f598b2e05112f8956fd424779b0
Daniel <danielvazquez245.dv@gmail.com>
2024-04-17 09:56:08 +0200

    2 new components created, structure defined

---
+++ components/task.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="border-b-2 ">
+    <div>
+      <h1> Titulo tarea</h1>
+    </div>
+
+    <div>
+      Descripcion de la tarea
+    </div>
+  </div>
+
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "task"
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="bg-teal-950 flex h-screen w-screen items-center justify-center  ">
     <div class=" max-w-[800px] w-full shadow-lg flex flex-col   ">
-
+      <div class="flex flex-col p-2 gap-2 bg-emerald-200 border-4 border-white rounded "> <!-- Contenedor tareas-->
+        <task/>
+      </div>
     </div>
   </div>
 </template>
